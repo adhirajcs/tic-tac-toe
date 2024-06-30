b20f1f58ba83ecbf3a4eeaff56aebb059a46e2eb
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-06-30 16:31:14 +0000

    Added Vercel Analytics and SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // import Home from './Home'
-import Board from './Extra/HomeReactDoc'
-import TicTacToe from './components/TicTacToe'
+import Board from "./Extra/HomeReactDoc";
+import TicTacToe from "./components/TicTacToe";
 
 // import './App.css'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path='' element={< TicTacToe />}/>
-        <Route path='/reactdocttt' element={< Board />}/>
-      </Routes>
+        <Routes>
+          <Route path="" element={<TicTacToe />} />
+          <Route path="/reactdocttt" element={<Board />} />
+        </Routes>
       </BrowserRouter>
+    <Analytics />
+    <SpeedInsights />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
