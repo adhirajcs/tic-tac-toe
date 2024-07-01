46ee13752776cd9fdc09c3ad84c39534af5daa84
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-07-01 15:29:10 +0000

    Fix

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -5,11 +5,11 @@ const GameOver = ({ gameState }) => {
     case GameState.inProgress:
       return <></>;
     case GameState.playerXWins:
-      return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 mt-12 text-xl">X Wins!</div>;
+      return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 w-60 mt-12 text-xl">X Wins!</div>;
     case GameState.playerOWins:
-      return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 mt-12 text-xl">O Wins!</div>;
+      return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 w-60 mt-12 text-xl">O Wins!</div>;
       case GameState.draw:
-        return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 mt-12 text-xl">Draw!!!</div>;
+        return <div className="game-over text-center border-blue-600 border-4 border-double p-2.5 w-60 mt-12 text-xl">Draw!!!</div>;
       default:
       return <></>;
   }
