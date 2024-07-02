253f61cfd023e05e705ebcb915155533bb472879
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-07-02 10:23:42 +0000

    Added different sounds

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -3,13 +3,15 @@ import Board from "./Board";
 import GameOver from "./GameOver";
 import GameState from "./GameState";
 import Reset from "./Reset";
-import gameOverSoundAsset from "../sounds/game_over.wav";
-import clickSoundAsset from "../sounds/click.wav";
+// import gameOverSoundAsset from "../sounds/game_over.wav";
+// import clickSoundAsset from "../sounds/click.wav";
+import gameOverSoundAsset from "../sounds/mixkit-achievement-bell.wav";
+import clickSoundAsset from "../sounds/mixkit-cool-interface-click-tone.wav";
 
 const gameOverSound = new Audio(gameOverSoundAsset);
 gameOverSound.volume = 0.2;
 const clickSound = new Audio(clickSoundAsset);
-clickSound.volume = 0.5;
+clickSound.volume = 0.1;
 
 const PLAYER_X = "X";
 const PLAYER_O = "O";
